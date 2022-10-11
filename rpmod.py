f65d49b348a30357a5a5ffa7021d4e7df42433c4
keeikd2 <109679869+keeikd2@users.noreply.github.com>
2022-10-11 18:52:20 +0300

    Update rpmod.py

--- rpmod.py
+++ rpmod.py
@@ -217,8 +217,8 @@ class RPMod(loader.Module):
 		nick = ''
 		for x in args:
 			for word in line_list: 
-                          if word in emoji.UNICODE_EMOJI:
-                            new_line_list.append(emoji.demojize(word))
+                if word in emoji.UNICODE_EMOJI:
+                new_line_list.append(emoji.demojize(word))
 			if x not in emoji.UNICODE_EMOJI['en'].keys(): nick+=x
 		if len(lst) > 3:
 			await utils.answer(message, f"Ник пользователя <b>{str(user.id)}</b> изменён на '<b>{args}</b>'")
